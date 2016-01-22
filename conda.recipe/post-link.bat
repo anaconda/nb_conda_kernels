360f58291fbe3e415b3b7740e2e4edf4f398827a
Nicholas Bollweg <nbollweg@continuum.io>
2016-01-22 11:19:48 -0500

    actually, not in Scripts. oy

--- conda.recipe/post-link.bat
+++ conda.recipe/post-link.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\python" -m nb_conda_kernels.install --enable --prefix="%PREFIX%"
+"%PREFIX%\python" -m nb_conda_kernels.install --enable --prefix="%PREFIX%"
--- conda.recipe/pre-unlink.bat
+++ conda.recipe/pre-unlink.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\python" -m nb_conda_kernels.install --disable --prefix="%PREFIX%"
+"%PREFIX%\python" -m nb_conda_kernels.install --disable --prefix="%PREFIX%"
