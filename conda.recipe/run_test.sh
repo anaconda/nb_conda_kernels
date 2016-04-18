d75a5cfe588449dea3593e01bae562fe83fc989d
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-18 16:33:51 -0400

    more testing work

--- conda.recipe/run_test.sh
+++ conda.recipe/run_test.sh
@@ -1,3 +1,4 @@
 "${PREFIX}/bin/npm" install -g npm@latest --no-progress --no-spin
 "${PREFIX}/bin/npm" install . --no-progress --no-spin
+"${PREFIX}/bin/npm" run lint
 "${PREFIX}/bin/npm" run test --no-progress --no-spin
