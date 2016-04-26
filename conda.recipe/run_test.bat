5cb31392d3c8877eb9d0883d4f66739f267eb7b8
Nicholas Bollweg <nbollweg@continuum.io>
2016-04-26 14:06:38 -0400

    use new normal npm, don't upgrade hot

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\npm.cmd" install -g npm@latest --no-spin && "%PREFIX%\Scripts\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\Scripts\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
+"%PREFIX%\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
