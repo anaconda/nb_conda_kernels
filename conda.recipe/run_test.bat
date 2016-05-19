ea2fc6cfef284beaeb327893b6a852dea09f79b7
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-19 16:19:09 -0400

    specify python

--- conda.recipe/run_test.bat
+++ conda.recipe/run_test.bat
@@ -1,1 +1,14 @@
-"%PREFIX%\npm.cmd" install . --no-spin --no-progress && "%PREFIX%\npm.cmd" run test --no-progress --no-spin && if errorlevel 1 exit 1
+pushd .
+cd /D %PREFIX%\..\..\pkgs
+(rmdir /s /q "\\?\%cd%\.trash" 2> NUL) || echo "some issues cleaning up"
+
+popd
+
+CALL npm install || EXIT /B 1
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+CALL npm run test || EXIT /B 1
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+REM force clean of pkg trash
+(rmdir /s /q "\\?\%cd%\node_modules" 2> NUL) || echo "some issues cleaning up"
