fc09f7f2710e50a8038a0e7ec7cd47ef6fb95eb1
Michael C. Grant <mcg@cvxr.com>
2018-08-23 20:19:12 -0500

    Fix the conda build path patcher

--- nb_conda_kernels/tests/test_runner.py
+++ nb_conda_kernels/tests/test_runner.py
@@ -1,16 +1,16 @@
-from os import environ, pathsep
-from sys import platform
+import os
+import sys
+
 from subprocess import check_output, CalledProcessError, STDOUT
 from nb_conda_kernels.manager import CondaKernelSpecManager
 
+is_win = sys.platform.startswith('win')
+
 
 def check_exec_in_env(key, argv):
-    is_win = platform.startswith('win')
     command = argv[:5]
     env_name = command[-1]
     env_name_fs = env_name.replace('\\', '/')
-    n_valid = 2
-    shell = is_win
     if key.endswith('-r'):
         command.extend(['Rscript', '-e',
                         'message(Sys.getenv("CONDA_PREFIX"));'
@@ -26,7 +26,7 @@ def check_exec_in_env(key, argv):
     except CalledProcessError as exc:
         com_out = exc.output
         valid = False
-    outputs = com_out.decode().splitlines()[-n_valid:]
+    outputs = com_out.decode().splitlines()
     if not (valid and len(outputs) >= 2 and
             all(o.strip() in (env_name, env_name_fs) for o in outputs[-2:])):
         print('Full output:\n--------\n{}--------'.format('\n'.join(outputs)))
@@ -34,16 +34,17 @@ def check_exec_in_env(key, argv):
 
 
 def test_runner():
-    if environ.get('CONDA_BUILD'):
+    if os.environ.get('CONDA_BUILD'):
         # Current versions of conda build invoke standard conda activation
         # *and* manually add the activation paths a second time. Unfortunately,
         # this frustrate's conda's ability to activate. This backs out the
         # redundancy so that the test can proceed.
-        path_list = environ['PATH'].split(pathsep)
-        indexes = [i for i, v in enumerate(path_list) if v == path_list[0]]
+        path_list = os.environ['PATH'].split(os.pathsep)
+        first_path = sys.prefix if is_win else os.path.join(sys.prefix, 'bin')
+        indexes = [i for i, v in enumerate(path_list) if v == first_path]
         if len(indexes) > 1:
             path_list = path_list[indexes[-1]:]
-            environ['PATH'] = pathsep.join(path_list)
+            os.environ['PATH'] = os.pathsep.join(path_list)
     spec_manager = CondaKernelSpecManager()
     for key, value in spec_manager._all_specs().items():
         if key.endswith('-py') or key.endswith('-r'):
