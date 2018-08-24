d876b389e4bae3ee9cd947bd23a6fc8990c8f7ef
Michael C. Grant <mgrant@anaconda.com>
2018-08-24 11:40:53 -0500

    Unicode madness

--- nb_conda_kernels/runner.py
+++ nb_conda_kernels/runner.py
@@ -3,7 +3,6 @@ import sys
 
 from subprocess import check_output, Popen
 
-
 def exec_in_env(conda_root, envname, command, *args):
     # Run the standard conda activation script, and print the
     # resulting environment variables to stdout for reading.
@@ -23,17 +22,16 @@ def exec_in_env(conda_root, envname, command, *args):
         ecomm = '. {} {} >/dev/null && printenv'.format(activate, envname)
         ecomm = ['bash', '-c', ecomm]
     env = check_output(ecomm, shell=is_win)
-    # Python 2 does not support Unicode environment strings
-    if sys.version_info[0] >= 3: 
-        env = env.decode()
-    env = .splitlines()
+    encoding = sys.stdout.encoding or 'utf-8'
+    env = env.decode(encoding).splitlines()
+    # print(type(env[-1]), type('='), type('@@@'))
 
     # Extract the path search results (Windows only). The "where"
     # command behaves like "which -a" in Unix, listing *all*
     # locations in the PATH where the executable can be found.
     # We need just the first.
     if is_win and not fullpath:
-        while not env[-1].startswith('@@@'):
+        while not env[-1].startswith(u'@@@'):
             fullpath = env.pop()
         env.pop()
         if not fullpath:
@@ -41,7 +39,11 @@ def exec_in_env(conda_root, envname, command, *args):
 
     # Extract the environment variables from the output, so we can
     # pass them to the kernel process.
-    env = dict(p.split('=', 1) for p in env if '=' in p)
+    env = dict(p.split(u'=', 1) for p in env if u'=' in p)
+    # Python 2 does not support unicode env dicts
+    if sys.version_info.major < 3:
+        env = {k.encode(encoding): v.encode(encoding)
+               for k, v in env.items()}
 
     # Launch the kernel process
     if is_win:
