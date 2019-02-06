0748dc6b948134740b2faa98012bc66cac582eab
Jonathan Helmus <jjhelmus@gmail.com>
2019-02-05 19:00:44 -0600

    Remove --prefix from post-link/pre-link scripts
    Explicitly passing --prefix to these scripts provide no benefit as this
    information can reliably determined from the python interpreter

--- conda-recipe/post-link.sh
+++ conda-recipe/post-link.sh
@@ -1,1 +1,1 @@
-"${PREFIX}/bin/python" -m nb_conda_kernels.install --enable --prefix ${PREFIX} >>"${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/python" -m nb_conda_kernels.install --enable >>"${PREFIX}/.messages.txt" 2>&1
--- conda-recipe/pre-unlink.bat
+++ conda-recipe/pre-unlink.bat
@@ -1,4 +1,4 @@
 @echo off
 :: Set PATH explicitly as it may not be set correctly by some versions of conda
 set "PATH=%PATH%;%PREFIX%\Library\bin"
-"%PREFIX%\python.exe" -m nb_conda_kernels.install --disable --prefix %PREFIX% >>"%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\python.exe" -m nb_conda_kernels.install --disable >>"%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
