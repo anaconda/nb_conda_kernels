096b4372df47b91de24d0ea088fe1b602635383d
Michael C. Grant <mgrant@anaconda.com>
2019-03-26 12:23:46 -0500

    Windows fixes

--- nb_conda_kernels/runner.py
+++ nb_conda_kernels/runner.py
@@ -2,22 +2,26 @@ from __future__ import print_function
 
 import os
 import sys
-import shlex
 import subprocess
+try:
+    from shlex import quote
+except ImportError:
+    from pipes import quote
 
 
 def exec_in_env(conda_root, envname, *command):
     # Run the standard conda activation script, and print the
     # resulting environment variables to stdout for reading.
-    envname = shlex.quote(envname)
-    cmd_string = ' '.join(map(shlex.quote, command))
     if sys.platform.startswith('win'):
-        activate = shlex.quote(os.path.join(conda_root, 'Scripts', 'activate.bat'))
-        ecomm = 'call {} {} & {}'.format(activate, envname, cmd_string)
+        command = subprocess.list2cmdline(command)
+        activate = os.path.join(conda_root, 'Scripts', 'activate.bat')
+        activator = subprocess.list2cmdline(['call', activate, envname])
+        ecomm = '{} & {}'.format(activator, command)
         subprocess.Popen(ecomm, shell=True).wait()
     else:
-        activate = shlex.quote(os.path.join(conda_root, 'bin', 'activate'))
-        ecomm = '. {} {} && exec {}'.format(activate, envname, cmd_string)
+        command = ' '.join(quote(c) for c in command)
+        activate = os.path.join(conda_root, 'bin', 'activate')
+        ecomm = ". '{}' '{}' && exec {}".format(activate, envname, command)
         ecomm = ['sh' if 'bsd' in sys.platform else 'bash', '-c', ecomm]
         os.execvp(ecomm[0], ecomm)
 
