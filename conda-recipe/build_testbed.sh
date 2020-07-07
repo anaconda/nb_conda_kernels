e7e6552d36bb2d2a4b6a05bf6899ffd7c0fd48fd
Michael C. Grant <mgrant@anaconda.com>
2020-07-07 14:10:34 -0500

    Refine testbed

--- conda-recipe/build_testbed.sh
+++ conda-recipe/build_testbed.sh
@@ -4,41 +4,51 @@
 [ "$RUNNER_OS" == "Windows" ] && CONDA_EXE="$CONDA/Scripts/conda.exe"
 [ "$RUNNER_OS" == "macOS" ] && export CONDA_PKGS_DIRS=~/.pkgs
 
+# Determine the root location of the testbed
 cwd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+[ $CONDA_ROOT ] || CONDA_ROOT=${cwd%/*}/conda
+mkdir -p $CONDA_ROOT
+export CONDA_ROOT=$(cd $CONDA_ROOT && pwd)
+echo "Testbed location: $CONDA_ROOT"
 
-if [[ "$CONDA_ROOT" != "" ]]; then
-    mkdir -p $CONDA_ROOT
-    croot=$(cd $CONDA_ROOT && pwd)
-else
-    croot=${cwd%/*}/conda
-fi
-
-# Exit quickly if the cached version is available. See the end of
-# the script for the explanation why we do it this way
-if [ -d $croot/conda-meta ]; then
-    source $croot/etc/profile.d/conda.sh
-    mkdir -p ~/.conda
-    cp $croot/environments.txt ~/.conda
+function finish {
+    # Populate the environments.txt list if it doesn't exist
+    # to make sure the off-location environment gets picked up
+    if [ ! -f ~/.conda/environments.txt ]; then
+        mkdir -p ~/.conda
+        echo $CONDA_ROOT/ext1/ext2/env/test_env1 >> ~/.conda/environments.txt
+    fi
     conda info
     conda info --envs
     exit 0
+}
+
+# Exit quickly if the cached version is available. See the end of
+# the script for the explanation why we do it this way
+if [ -d $CONDA_ROOT/conda-meta ]; then
+    source $CONDA_ROOT/etc/profile.d/conda.sh
+    conda activate base
+    finish
 fi
 
 # Create the root environment
-${CONDA_EXE:-conda} create -y -p $croot conda python=3.7
+${CONDA_EXE:-conda} env create -f $cwd/croot.yml -p $CONDA_ROOT
+source $CONDA_ROOT/etc/profile.d/conda.sh
+conda activate base
+echo "CONDA_ROOT: "$CONDA_ROOT
+echo "CONDA_PREFIX: "$CONDA_PREFIX
+pip install -e .
+python -m nb_conda_kernels.install --enable
 
 # We need to create additional environments to fully test the logic,
 # including an R kernel, a Python kernel, and environment names with at
 # least one non-ASCII character and one space. We also need one environment
 # installed in a non-default environment location.     
-source $croot/etc/profile.d/conda.sh
-conda install -y conda-build conda-verify ipykernel
 conda env create -f $cwd/testenv1.yaml
 conda env create -f $cwd/testenv2.yaml
 mkdir -p $CONDA_ROOT/ext1/ext2/env
 conda env create -f $cwd/testenv1.yaml -p $CONDA_ROOT/ext1/ext2/env/test_env1
-rm -rf $croot/pkgs
+rm -rf $CONDA_ROOT/pkgs
 
 # Display final result
-conda info
-conda info --envs
+finish
