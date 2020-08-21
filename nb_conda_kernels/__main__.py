d1f309382424f2abc97d7565ad448cdf10e55a14
Frédéric Collonval <frederic.collonval@ariadnext.com>
2020-08-21 12:06:44 +0200

    Use parent `JupyterApp` to share the configuration

--- nb_conda_kernels/__main__.py
+++ nb_conda_kernels/__main__.py
@@ -1,5 +1,7 @@
 from jupyter_client import kernelspec
 from .manager import CondaKernelSpecManager
 kernelspec.KernelSpecManager = CondaKernelSpecManager
+
 from jupyter_client.kernelspecapp import KernelSpecApp  # noqa
+
 KernelSpecApp.launch_instance()
