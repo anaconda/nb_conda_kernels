6101c086a122d9e3b7833ca7cbc26e91d0a08130
Bas Nijholt <bas@nijho.lt>
2024-02-29 10:23:58 -0800

    Revert change made by `versioneer install`

--- nb_conda_kernels/__init__.py
+++ nb_conda_kernels/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from .manager import CondaKernelSpecManager
-from . import _version
-__version__ = _version.get_versions()['version']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
