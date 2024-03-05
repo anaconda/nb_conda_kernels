733c0d662d2790d6e8f6012e5ce23b2f3b54ce8f
Michael C. Grant <mgrant@anaconda.com>
2024-03-05 09:28:07 -0600

    experiment with a true noarch

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,9 +1,14 @@
 %PYTHON% -m pip install --no-deps --ignore-installed .
 if errorlevel 1 exit 1
 
-set SCRIPTS="%PREFIX%\Scripts"
-if not exist %SCRIPTS% mkdir %SCRIPTS%
+set SCRDIR="%PREFIX%\Scripts"
+if not exist %SCRDIR% mkdir %SCRDIR%
 if errorlevel 1 exit 1
 
-copy %SRC_DIR%\conda-recipe\post-link.bat %SCRIPTS%\.nb_conda_kernels-post-link.bat || exit 1
-copy %SRC_DIR%\conda-recipe\pre-unlink.bat %SCRIPTS%\.nb_conda_kernels-pre-unlink.bat || exit 1
+POST_LINK="%SCRDIR%\.nb_conda_kernels-post-link"
+PRE_UNLINK="%SCRDIR%\.nb_conda_kernels-pre-unlink"
+
+copy "%SRC_DIR%\conda-recipe\post-link.bat" "%POST_LINK%.bat" || exit 1
+copy "%SRC_DIR%\conda-recipe\pre-unlink.bat" "%PRE_UNLINK%.bat" || exit 1
+copy "%SRC_DIR%\conda-recipe\post-link.sh" "%POST_LINK%.sh" || exit 1
+copy "%SRC_DIR%\conda-recipe\pre-unlink.sh" "%PRE_UNLINK%.sh" || exit 1
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,11 +1,12 @@
 $PYTHON -m pip install --no-deps --ignore-installed .
 
-mkdir -p "$PREFIX"/bin
+BINDIR="$PREFIX/bin"
+mkdir -p "$BINDIR"
 
-POST_LINK="$PREFIX"/bin/.nb_conda_kernels-post-link.sh
-PRE_UNLINK="$PREFIX"/bin/.nb_conda_kernels-pre-unlink.sh
+POST_LINK="$BINDIR/.nb_conda_kernels-post-link"
+PRE_UNLINK="$BINDIR/.nb_conda_kernels-pre-unlink"
 
-cp "$SRC_DIR"/conda-recipe/post-link.sh "$POST_LINK"
-cp "$SRC_DIR"/conda-recipe/pre-unlink.sh "$PRE_UNLINK"
-
-chmod +x "$POST_LINK" "$PRE_UNLINK"
+cp "$SRC_DIR/conda-recipe/post-link.sh" "$POST_LINK.sh"
+cp "$SRC_DIR/conda-recipe/pre-unlink.sh" "$PRE_UNLINK.sh"
+cp "$SRC_DIR/conda-recipe/post-link.bat" "$POST_LINK.bat"
+cp "$SRC_DIR/conda-recipe/pre-unlink.bat" "$PRE_UNLINK.bat"
